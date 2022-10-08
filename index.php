42d50e9cfc39972b72924066b63f36078c4a14ad
NicoleDuran23 <69700461+NicoleDuran23@users.noreply.github.com>
2022-10-07 22:04:52 -0400

    Update index.php

--- index.php
+++ index.php
@@ -8,8 +8,9 @@
         <meta name="Diseño y Publicidad" content="">
         <meta name="Central de Diseño" content="">
         <link rel="icon" href="img/favicon.ico">
-        <title>Biblioteca Musical</title>
+        <title>Login Usuario</title>
         <!-- Bootstrap core CSS -->
+        <!-- Latest compiled and minified CSS -->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
@@ -17,31 +18,73 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
   </head>
 <body>
-<div class="container">
+    <div class="container">
                 <div class="row">
                     <div class="col-md-2"></div>
                     <div class="col-md-8"><font color="#000"><center><strong><h1>BIBLIOTECA</h1></strong><h3>MUSICAL</h3></center></font></div>
                     <div class="col-md-2"></div>
                 </div>
-                <hr>
-    </div>
-    <div class="row">
-        <div class="col-md-4"></div>
-        <div class="col-md-4">
-        <div class="well">
-            <center>
-                <h3><strong>BIENVENIDO</strong></h3><br>
-                <img src="usfa.png" class="img-circle" width="150" height="150">
-                <br><br><br>
-                <form class="form-inline"  method ="POST" name="inicio">
-                    <a class ="btn btn-success" href="login_usu.php" role="button" name="btn_login_usu">LOGIN USUARIO</a><br><br>
-                    <a class ="btn btn-success" href="login_admi.php" role="button" name="btn_login_admi">LOGIN ADMINISTRADOR</a><br><br>
-                    <a class ="btn btn-success" href="registro_usu.php" role="button" name="btn_registro_usu">REGISTRATE</a><br><br>
-                    <a class ="btn btn btn-danger" href="http://www.google.com" role="button" name="btn_salir">SALIR</a><br><br>
-                </form>
-            </center>
+        <hr>
+        <h3>
+            <p class="bg-danger" align="center">
+                <b>
+                    <?php
+                    session_start();
+                    ob_start();
+                        if($_SESSION['sesion_exito']==2)
+                        {
+                            echo "Los campos SON OBLIGATORIOS";
+                        }
+                        if($_SESSION['sesion_exito']==3)
+                        {
+                            echo "DATOS INCORRECTOS";
+                        }
+                        else {
+                            $_SESSION['sesion_exito']=0;
+                        }
+                    
+                    ?>
+                </b>
+            </p>
+            <p class="bg-succes" align="center">
+                <b>
+                    <?php
+                   /* if($_SESSION['sesion_exito']==0){
+                        echo "GRACIAS POR USAR NUESTROS SERVICIOS";
+                    }
+                    $_SESSION['sesion_exito']=0;*/
+                    ?>
+                </b>
+            </p>
+        </h3>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4">
+                <div class="well">
+                    <center>
+                        <h3><strong>INICIAR SESION</strong></h3><br>
+                        <img src="usfa.png" class="img-circle" width="150" height="150">
+                        <br><br><br>
+
+                        <form class="form-inline" action="login.php" method="POST" name="login_usu">
+                            <div class="form-group">
+                                <label for="usuario">USUARIO</label>
+                                     &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                                     <input type="text" class="form-control" id="usuario" placeholder="Usuario" name="usuario">
+                            </div>
+                            <br><br>
+                            <div class="form-group">
+                                <label for="pass">CONTRASEÑA</label>
+                                <input type="password" class="form-control" id="pass" placeholder="Contraseña" name="pass">
+                            </div>
+                            <br><br>
+                            <input type="hidden" name="envio">
+                            <p><input type="submit" id="enviar" class="btn btn-success" value="INICIAR SESIÓN" name="btn_log_usu">
+                            <a class="btn btn btn-danger" href="http://www.google.com"  role="button">SALIR</a></p></p>
+                        </form>
+                    </center>
+                </div>
+            </div>
         </div>
-        </div>
-    </div>
 </body>
 </html>
