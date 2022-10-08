a58f56656dc0c04f3215773258e542f38082ce96
NicoleDuran23 <69700461+NicoleDuran23@users.noreply.github.com>
2022-10-07 22:11:10 -0400

    Update login.php

--- login.php
+++ login.php
@@ -48,7 +48,6 @@
     <div class="container">
         <div class="row">
         <div class="col-md-2"></div>
-                    <div class="col-md-8"><font color="#000"><center><strong><h1>BIBLIOTECA</h1></strong><h3>MUSICAL</h3></center></font></div>
                     <div class="col-md-2"></div>
         </div>
         <hr>
